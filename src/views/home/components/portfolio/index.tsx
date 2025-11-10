7e5f5f1c6aec2fece6dc3b454c60c777cdc742a5
Kodeneko <ladysun.freedom@gmail.com>
2025-11-10 14:04:40 +0100

    [secs] portfolio arrows

--- src/views/home/components/portfolio/index.tsx
+++ src/views/home/components/portfolio/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import ButtonSq from '../../../../components/button-sq';
@@ -7,23 +7,33 @@ import { portfolioList } from '../../../../globals/portfolio';
 import styles from './styles.module.less';
 import type { PortfolioSecProps } from './types';
 
+const HALF_CARD = 300 / 2;
+
 const PortfolioSec: React.FC<PortfolioSecProps> = () => {
   const { t } = useTranslation();
+  const [move, setMove] = useState(0);
+  const handleArrow = (direction: number) => {
+    if (direction === -1 && move === 0)
+      return;
+    setMove(move + direction);
+  };
+  const calcMove = (move: number) => move * HALF_CARD * (-1);
+
   return (
     <div className={styles.cont}>
       <div className={styles.header}>
         <h2 className='h2HeadPac'>{ t('portfolio.title') }</h2>
         <div className={styles.btns}>
-          <ButtonSq icon="fa-solid fa-caret-left" />
-          <ButtonSq icon="fa-solid fa-caret-right" />
+          <ButtonSq onClick={() => handleArrow(-1)} icon="fa-solid fa-caret-left" />
+          <ButtonSq onClick={() => handleArrow(1)} icon="fa-solid fa-caret-right" />
         </div>
       </div>
       <div className={styles.projects}>
-        <div className={styles.inner}>
+        <div className={styles.inner} style={{ transform: `translateX(${calcMove(move)}px)` }}>
           {portfolioList.map(p =>
             <Card
               key={p.id}
-              pic={p.pic }
+              pic={p.pic}
               title={t(`portfolio.${p.id}.title`)}
               body={t(`portfolio.${p.id}.desc`)}
               actions={p.links}
